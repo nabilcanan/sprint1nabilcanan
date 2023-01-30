1d4404a92a49b35cf4459d627d97f3213f2471c6
nabilcanan <nabiicanan78@gmail.com>
2023-01-29 21:16:55 -0500

    here

--- nabilcanansprint1/main.py
+++ nabilcanansprint1/main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import json
+import sys
+import requests
+from secrets import wufoo_key
+from requests.auth import HTTPBasicAuth
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_wufoo_data() -> dict:
+    url = "http://{subdomain}.wufoo.com/api/v3/forms/{identifier}/entries/count.{format}"
+    response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, 'pass'))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if response.status_code != 200:  # if we don't get an ok response we have trouble
+        print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason} ")
+        sys.exit(-1)
+    jsonresponse = response.json()
+    return jsonresponse  # json response will be either a dictionary or a list of dictionaries
+
+
+def write_wufu_data():
+    data = get_wufoo_data()['Entries']
+
+    with open("data.txt", "w") as outfile:
+        # write the JSON key-value pairs to the file
+        for item in data:
+            for key, value in item.items():
+                outfile.write(f"{key}: {value}\n")
+
+    print_file("datafile.txt")
+
+
+def print_file(file_name):
+    with open(file_name) as f:
+        print(f.read())
+
+
+if __name__ == "__main__":
+    write_wufu_data()
